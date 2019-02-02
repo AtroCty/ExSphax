0cab5af8cb62d179b1bd80db72130fc2eb752b85
Timm Schütte <atrocty@gmx.de>
2019-02-02 21:13:55 +0100

    20190202_Node_Update

--- 3_Move.bat
+++ 3_Move.bat
@@ -1,6 +1,4 @@
-@RD /S /Q "%CD%\com.atrocty.ExSphax"
-robocopy "C:\Users\timms\AppData\Roaming\Adobe\CEP\extensions\com.atrocty.ExSphax" "E:\Coding\Eclipse PS\com.atrocty.ExSphax" /E /MOVE
-robocopy "E:\Coding\Eclipse PS\manifest.xml" "E:\Coding\Eclipse PS\com.atrocty.ExSphax\CSXS"
-del "E:\Coding\Eclipse PS\ExSphax.zxp"
+@RD /S /Q "%AppData%\Adobe\CEP\extensions\com.atrocty.ExSphax"
+robocopy "%CD%\com.atrocty.ExSphax" "%AppData%\Adobe\CEP\extensions\com.atrocty.ExSphax" /E
+del "%CD%\ExSphax.zxp"
 pause
-cmd /k
